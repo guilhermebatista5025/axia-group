4ee66264cf121a54252d63e38397d309bcf4f0ac
Guilherme Batista <gmito5025@gmail.com>
2026-06-24 23:08:27 -0300

    to tentando terminar o front kaakakak

--- print_lines.py
+++
@@ -1,6 +0,0 @@
-with open(r"c:\Users\Aluno\Downloads\axia-group\assets\css\styles.css", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-for i in range(149, 220):
-    if i < len(lines):
-        print(f"Line {i+1}: {lines[i]}", end="")
